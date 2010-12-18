a7b9bf2e5d061b16b71537ee8c0c3438912b3203
B V Satyaram <bvsatyaram@gmail.com>
2010-12-19 00:53:38 +0530

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BunkTracker::Application.config.secret_token = '80cc97683a7cd8aab7cc1eafa2ec963d8bd46247d94699ca04bcea1075430257265f3c0a4b92ce51e8ee9c8e6af5ba02b3772cc8eed6450b39e67b7a03074d01'
